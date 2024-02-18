f3c54ae7c99b85bbcfac960c76d0130bae4dbcd2
Thibaut <thibaut.moncourtois@gmail.com>
2024-02-18 13:44:41 +0100

    Set up basic routing

--- backend/routes/users.route.js
+++ backend/routes/users.route.js
@@ -8,11 +8,22 @@ router.get('/', (req, res) => {
 
 //GET: On récupère un user par son ID
 router.get('/:id', (req, res) => {
-  res.json({ message: 'Voici un User par son ID'})
+  res.json({ message: 'Voici un User par son ID' })
 })
 
 //POST: Inscription d'un nouvel user
+router.post('/', (req, res) => {
+  res.json({ message: 'Bienvenue nouvel utilisateur!' })
+})
 
+//PUT: Afin de modifier un user
+router.put('/:id', (req, res) => {
+  res.json({ message: 'Modification effectuée!' })
+})
 
+//DELETE: Supprimer un user par son ID 
+router.delete('/:id', (req, res) => {
+  res.json({ message: 'Compte supprimé!' })
+})
 
 module.exports = router
